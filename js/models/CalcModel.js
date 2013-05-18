62312cf3b8f16b5525b4badc8d8540612ad7f02a
Alexander Vasilenko <vasilenko_alex@odesk.com>
2013-05-18 05:06:26 +0300

    Add some useless code

--- js/models/CalcModel.js
+++ js/models/CalcModel.js
@@ -5,7 +5,25 @@ define([
 
     return Backbone.Model.extend({
 
-        defaults: {}
+        defaults: {
+            result: 0
+        },
+
+        plus: function (arg) {
+            this.set('result', this.get('result') + parseFloat(arg));
+        },
+
+        minus: function (arg) {
+            this.set('result', this.get('result') - parseFloat(arg));
+        },
+
+        multiply: function (arg) {
+            this.set('result', this.get('result') * parseFloat(arg));
+        },
+
+        divide: function (arg) {
+            this.set('result', this.get('result') / parseFloat(arg));
+        }
 
     });
 
--- js/views/CalcView.js
+++ js/views/CalcView.js
@@ -2,14 +2,35 @@ define([
     'jquery',
     'underscore',
     'backbone',
+    'models/CalcModel',
     'text!templates/calcTemplate.html'
-], function ($, _, Backbone, calcTemplate) {
+], function ($, _, Backbone, CalcModel, calcTemplate) {
 
     return Backbone.View.extend({
         el: $("#main"),
 
+        initialize: function () {
+            this.model = new CalcModel({result: 0});
+        },
+
         render: function () {
-            this.$el.html(calcTemplate);
+            var self = this;
+
+            // Render template
+            self.$el.html(calcTemplate);
+
+            var $screen = self.$el.find('.screen');
+
+            // Refresh screen
+            self.model.on('change:result', function () {
+                $screen.text(self.model.get('result'));
+            });
+
+            // Add events
+            self.$el.on('click', '.digit', function (event) {
+                var $element = $(event.currentTarget);
+                $screen.text($element.text());
+            });
         }
 
     });
